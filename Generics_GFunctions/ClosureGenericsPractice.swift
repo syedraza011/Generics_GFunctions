99d2b3ab61dba448386450508323cd3c4f0afb24
Syed Raza <syed.raza011@gmail.com>
2023-07-02 11:26:33 -0400

    First push

---
+++ Generics_GFunctions/ClosureGenericsPractice.swift
@@ -0,0 +1,8 @@
+//
+//  ClosureGenericsPractice.swift
+//  Generics_GFunctions
+//
+//  Created by Syed Raza on 7/2/23.
+//
+
+import Foundation
---
+++ Generics_GFunctionsTests/GenericTests.swift
@@ -0,0 +1,10 @@
+//
+//  GenericTests.swift
+//  Generics_GFunctionsTests
+//
+//  Created by Syed Raza on 7/2/23.
+//
+
+import Foundation
+import XCTest
+@testable import Generics_GFunctions
