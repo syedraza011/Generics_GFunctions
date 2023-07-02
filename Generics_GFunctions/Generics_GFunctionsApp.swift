23be892a70fd3f31c1cd985ab1480b40c7595dfc
Syed Raza <syed.raza011@gmail.com>
2023-07-02 11:22:54 -0400

    Initial Commit

---
+++ Generics_GFunctions/Generics_GFunctionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Generics_GFunctionsApp.swift
+//  Generics_GFunctions
+//
+//  Created by Syed Raza on 7/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Generics_GFunctionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
